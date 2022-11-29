2177b40ff700c47db61f061648ff924a3a42fba7
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-29 15:18:05 +0500

    Added HubChains

--- funds/orm/base/__init__.py
+++ funds/orm/base/__init__.py
@@ -1,1 +1,2 @@
 from funds.orm.models.hubs.h_addresses import HubAddresses
+from funds.orm.models.hubs.h_chains import HubChains
--- funds/orm/models/hubs/h_addresses.py
+++ funds/orm/models/hubs/h_addresses.py
@@ -8,6 +8,6 @@ class HubAddresses(Base):
 
     __tablename__ = 'h_addresses'
 
-    h_address_id = Column(Integer, primary_key=True)  # PK
+    h_address_id = Column(Integer, primary_key=True)
     h_address = Column(Text, unique=True, nullable=False)
     h_address_load_ts = Column(DateTime, server_default=func.now(), nullable=False)
