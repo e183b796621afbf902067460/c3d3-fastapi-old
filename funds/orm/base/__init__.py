02f233e0db94d96723820f61fdd580826a166944
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-29 15:21:03 +0500

    Added LinkAddressesLabelsChains

--- funds/orm/base/__init__.py
+++ funds/orm/base/__init__.py
@@ -3,3 +3,4 @@ from funds.orm.models.hubs.h_chains import HubChains
 from funds.orm.models.hubs.h_labels import HubLabels
 
 from funds.orm.models.links.l_addresses_chains import LinkAddressesChains
+from funds.orm.models.links.l_addresses_labels_chains import LinkAddressesLabelsChains
