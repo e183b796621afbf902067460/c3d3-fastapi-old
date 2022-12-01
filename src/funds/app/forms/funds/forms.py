7a984e11a3f3dd257185a18a1975ed1d8b6722d6
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-02 04:21:53 +0500

    Removed unnecessary files

--- src/funds/app/forms/funds/forms.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class LabelForm(BaseModel):
-    name: str
-    password: str
-
--- src/funds/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=funds/:/home/user/repositories/defi-microservice-funds/
-/home/user/repositories/defi-microservice-funds/venv/bin/uvicorn funds.main:app --host 127.0.0.1 --port 8080 --reload
