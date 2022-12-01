d364065a05366296ce2ac75a62a33e3e41a3d67a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-02 04:24:10 +0500

    Updated FundCBV

--- src/funds/app/views/funds/view.py
+++ src/funds/app/views/funds/view.py
@@ -1,59 +1,36 @@
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
-from fastapi import Depends, HTTPException, status
-from fastapi.responses import RedirectResponse
+from fastapi import Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
 
-from src.funds.orm.cfg.engine import settings
-from src.funds.app.services.funds.service import LabelBase
-from src.funds.app.forms.funds.forms import LabelForm
-
+from src.funds.app import schemas
+from src.funds.app.services.funds.service import FundService
 
 router = InferringRouter()
 
 
 @cbv(router=router)
-class LabelCBV(LabelBase):
-    _session = Depends(settings.get_session)
+class FundCBV:
 
-    @router.post("/sign-up")
-    def on_post__label_sign_up(self, label: LabelForm):
-        db_label = self._get_label_by_name(label=label.name)
-        if db_label:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Label already registered, try to sign-in'
-            )
-        model = self._create_label(label=label)
+    @router.post(
+        "/sign-up",
+        response_model=schemas.funds.TokenSchema,
+        status_code=status.HTTP_201_CREATED
+    )
+    def on_post__label_sign_up(self, fund_create_schema: schemas.funds.FundCreateSchema, service: FundService = Depends()):
+        return service.on_post__label_sign_up(fund_create_schema=fund_create_schema)
 
-        return RedirectResponse(
-            f'/account/{model.h_label_name}',
-            status_code=status.HTTP_302_FOUND
-        )
+    @router.post(
+        "/sign-in",
+        response_model=schemas.funds.TokenSchema,
+        status_code=status.HTTP_200_OK,
+    )
+    def on_post__label_sign_in(self, oauth2_: OAuth2PasswordRequestForm = Depends(), service: FundService = Depends()):
+        return service.on_post__label_sign_in(oauth2_=oauth2_)
 
-    @router.post("/sign-in")
-    def on_post__label_sign_in(self, label: LabelForm):
-        db_label = self._get_label_by_name(label=label.name)
-        if not db_label:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Label is not registered, try to sign-up'
-            )
-        if not self._validate_password(password=label.password, hashed_password=db_label.h_label_password):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Wrong password, try again'
-            )
-        return RedirectResponse(
-            f'/account/{db_label.h_label_name}',
-            status_code=status.HTTP_302_FOUND
-        )
-
-    @router.get("/account/{label}")
-    def on_get__label_account(self, label: str):
-        db_label = self._get_label_by_name(label=label)
-        if not db_label:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Inactive token'
-            )
-        return db_label
+    @router.get(
+        "/account",
+        response_model=schemas.funds.FundORMSerializeSchema
+    )
+    def on_get__label_account(self, fund: schemas.funds.FundORMSerializeSchema = Depends(FundService.get_user_by_jwt_token)):
+        return fund
