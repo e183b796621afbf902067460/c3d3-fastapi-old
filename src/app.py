84d9d4400026f7741eac9a5daeb732839c572e51
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-04 04:23:27 +0500

    Added redirect middleware

--- src/app.py
+++ src/app.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from starlette.responses import RedirectResponse
 
-from src.funds.app.views.funds.view import router as funds_router
+from src.funds.app.views.labels.view import router as funds_router
+from src.funds.app.views.wallets.view import router as wallets_router
 
 
-app: FastAPI = FastAPI(openapi_url='/auth/sign-in')
+app: FastAPI = FastAPI()
 
-app.include_router(funds_router, prefix='/auth', tags=['Auth'])
+
+@app.exception_handler(status.HTTP_401_UNAUTHORIZED)
+def redirect(*args, **kwargs):
+    return RedirectResponse(url='/funds/sign-in')
+
+
+app.include_router(funds_router, prefix='/funds', tags=['Auth'])
+app.include_router(wallets_router, prefix='/wallets', tags=['Wallets'])
