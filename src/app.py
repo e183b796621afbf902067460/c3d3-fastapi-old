b502994ac36a72e0aa1d8636be266cb11fbaf87a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-04 03:03:45 +0500

    Added instruments microservice

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-from src.funds.app.views.funds.view import router
+from src.funds.app.views.funds.view import router as funds_router
 
 
-app: FastAPI = FastAPI()
+app: FastAPI = FastAPI(openapi_url='/auth/sign-in')
 
-app.include_router(router, prefix='/auth', tags=['Auth'])
+app.include_router(funds_router, prefix='/auth', tags=['Auth'])
