38df344c3ba391dca9d60dd72b3f9e9e28ab1078
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-06 15:55:06 +0500

    Updated WalletCBV

--- src/funds/app/views/wallets/view.py
+++ src/funds/app/views/wallets/view.py
@@ -1,6 +1,6 @@
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
-from fastapi import Depends, status
+from fastapi import Depends, status, HTTPException
 
 from typing import List
 
@@ -22,3 +22,29 @@ class WalletCBV:
     )
     def on_get__wallets_all(self, label: schemas.labels.LabelORMSchema = Depends(current_label), service: WalletService = Depends()):
         return service.on_get__wallets_all(label=label.h_label_name)
+
+    @router.post(
+        '/add',
+        response_model=schemas.wallets.WalletORMSchema,
+        status_code=status.HTTP_201_CREATED
+    )
+    def on_post__wallets_add(self, wallet_add_schema: schemas.wallets.WalletAddSchema, label: schemas.labels.LabelORMSchema = Depends(current_label), service: WalletService = Depends()):
+        wallet: schemas.wallets.WalletORMSchema = service.on_post__wallets_add(
+            address=wallet_add_schema.address,
+            chain=wallet_add_schema.chain,
+            label=label.h_label_name
+        )
+        if not wallet:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Failed parameters'
+            )
+        return wallet
+
+    @router.get(
+        '/{wallet_address}',
+        status_code=status.HTTP_200_OK,
+        response_model=schemas.wallets.WalletORMSchema
+    )
+    def on_get__wallets_fetchone(self, wallet_address: str, service: WalletService = Depends(), label: schemas.labels.LabelORMSchema = Depends(current_label)):
+        ...
