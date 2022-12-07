e6f77915bb8a9751c6060cc0c965719af1e357a6
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-07 16:18:00 +0500

    Added HubProtocolsCategories

--- src/orm/base/__init__.py
+++ src/orm/base/__init__.py
@@ -1,6 +1,8 @@
 from src.orm.models.hubs.h_addresses import HubAddresses
 from src.orm.models.hubs.h_chains import HubChains
 from src.orm.models.hubs.h_labels import HubLabels
+from src.orm.models.hubs.h_protocol_category import HubProtocolsCategories
+from src.orm.models.hubs.h_protocols import HubProtocols
 
 from src.orm.models.links.l_addresses_chains import LinkAddressesChains
 from src.orm.models.links.l_addresses_labels_chains import LinkAddressesLabelsChains
