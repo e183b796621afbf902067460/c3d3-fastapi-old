68d3cd90d96b54d8baca934f7a7e2c67dbc1b2fd
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-08 04:38:02 +0500

    Updated DB architecture

--- src/orm/models/links/l_addresses_protocols_categories_chains.py
+++ src/orm/models/links/l_addresses_protocols_categories_chains.py
@@ -13,3 +13,4 @@ class LinkAddressesProtocolsCategoriesChains(Base):
     l_address_protocol_category_chain_id = Column(Integer, primary_key=True)
     l_protocol_category_chain_id = Column(Integer, ForeignKey('l_protocols_categories_chains.l_protocol_category_chain_id'), nullable=False)
     l_address_chain_id = Column(Integer, ForeignKey('l_addresses_chains.l_address_chain_id'), nullable=False)
+    l_address_label_chain_id = Column(Integer, ForeignKey('l_addresses_labels_chains.l_address_label_chain_id'), nullable=False)
