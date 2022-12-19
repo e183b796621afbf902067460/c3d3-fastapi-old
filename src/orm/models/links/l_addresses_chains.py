17da972814a0f74bc696a5a65fdcd84e12904781
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-20 04:58:49 +0500

    Added _fixtures

--- src/orm/models/links/l_addresses_chains.py
+++ src/orm/models/links/l_addresses_chains.py
@@ -14,4 +14,4 @@ class LinkAddressesChains(Base):
     h_address_id = Column(Integer, ForeignKey('h_addresses.h_address_id'), nullable=False)
     h_chain_id = Column(Integer, ForeignKey('h_chains.h_chain_id'), nullable=False)
 
-    l_address_chain_name = Column(Text)
+    l_address_chain_name = Column(Text, nullable=True)
